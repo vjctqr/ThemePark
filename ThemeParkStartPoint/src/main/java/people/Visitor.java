12f21ffbece828f0b9b496f36ea3eb339bc5ecf4
Victor <victorsetlhare@gmail.com>
2020-06-23 21:23:05 +0100

    initial commit

---
+++ ThemeParkStartPoint/src/main/java/people/Visitor.java
@@ -0,0 +1,28 @@
+package people;
+
+public class Visitor {
+
+    checking whats wrong
+
+    private int age;
+    private double height;
+    private double money;
+
+    public Visitor(int age, double height, double money) {
+        this.age = age;
+        this.height = height;
+        this.money = money;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+}
